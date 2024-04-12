375a01040e6a3f3fd2eb1cbadf21882d308cfa1e
Mike Zhang <mikexd.dev@gmail.com>
2024-04-12 14:47:05 +0800

    stage

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -11,60 +11,81 @@ import {
   fetchFeedByFids,
   fetchAndProcessFeeds,
 } from "@/app/lib/farcaster";
+import { questions } from "@/app/data";
 
-const app = new Frog({
+type State = {
+  user: any;
+  userCastsData: any;
+  qn_one: string;
+  qn_two: string;
+  qn_three: string;
+  qn_four: string;
+};
+
+const app = new Frog<{ State: State }>({
   assetsPath: "/",
   basePath: "/api",
   // Supply a Hub to enable frame verification.
   // hub: neynar({ apiKey: "NEYNAR_FROG_FM" }),
+  initialState: {
+    userCastsData: [],
+    user: {},
+    qn_one: "",
+    qn_two: "",
+    qn_three: "",
+    qn_four: "",
+  },
 });
 
-let qna = [{
+let result = {
+  userCastsData: [],
+  user: {},
+  qn_one: "",
+  qn_two: "",
+  qn_three: "",
+  qn_four: "",
+};
+let qna = [
+  {
     question: "How do you use Warpcast?",
-    answer:""
+    answer: "",
   },
   {
     question: "What's your posting style?",
-    answer:""
+    answer: "",
   },
   {
     question: "What's your main content?",
-    answer:""
+    answer: "",
   },
   {
     question: "How do others describe you?",
-    answer:""
-  }];
+    answer: "",
+  },
+];
 
 // Uncomment to use Edge Runtime
 // export const runtime = 'edge'
 
 app.frame("/", async (c) => {
-  const { buttonValue, inputText, status } = c;
-  const fruit = inputText || buttonValue;
-  const user = await fetchUser(5650);
-  const feeds = await fetchAndProcessFeeds(5650);
-  console.log(feeds, "feeds");
+  const { buttonValue, inputText, status, deriveState } = c;
+  // const fruit = inputText || buttonValue;
+  // console.log(feeds, "feeds");
+  const user = (await fetchUser(5650))?.result?.user;
+  console.log(user, "user");
+  const feeds: any = await fetchAndProcessFeeds(5650);
+  const state = await deriveState(async (previousState) => {
+    console.log(state, "in the state");
+    previousState.user = user;
+    previousState.userCastsData = feeds;
+  });
+  result.user = user;
+  result.userCastsData = feeds;
   // TODO: ai stuff
 
   console.log(c, "c");
   return c.res({
-    action: "/picker",
-    image: "https://i.kym-cdn.com/entries/icons/mobile/000/032/280/meme1.jpg",
-    intents: [
-      <Button value="A">A</Button>,
-      <Button value="B">B</Button>,
-      // status === "response" && <Button.Reset>Reset</Button.Reset>,
-    ],
-  });
-});
-
-app.frame("/picker", (c) => {
-  const { buttonValue, inputText, status } = c;
-  const fruit = inputText || buttonValue;
-  console.log(c, "c2");
-  return c.res({
-    // action: "/picker",
+    action: "/q1",
     image: (
       <div
         style={{
@@ -83,6 +104,7 @@ app.frame("/picker", (c) => {
           width: "100%",
         }}
       >
+        {/* <img src="https://i.kym-cdn.com/entries/icons/mobile/000/032/280/meme1.jpg" /> */}
         <div
           style={{
             color: "white",
@@ -96,21 +118,55 @@ app.frame("/picker", (c) => {
             whiteSpace: "pre-wrap",
           }}
         >
-          hello
+          Welcome, <br />
+          {user.displayName}!
         </div>
-      </div>),
+        <div
+          style={{
+            color: "white",
+            fontSize: 30,
+            fontStyle: "normal",
+            letterSpacing: "-0.025em",
+            lineHeight: 1.4,
+            marginTop: 30,
+            display: "flex",
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          Want to know what kind of farcasters are you?
+        </div>
+        <div
+          style={{
+            color: "yellow",
+            fontSize: 20,
+            fontStyle: "italic",
+            letterSpacing: "-0.025em",
+            lineHeight: 1.4,
+            marginTop: 60,
+            display: "flex",
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          (Do wait for your username to appear)
+        </div>
+      </div>
+    ),
     intents: [
-      <Button value="A">Yes</Button>,
+      // <Button value="A">A</Button>,
+      <Button value={"continue"}>Continue</Button>,
       // status === "response" && <Button.Reset>Reset</Button.Reset>,
     ],
   });
 });
 
 app.frame("/q1", (c) => {
-  const { buttonValue, status } = c;
+  const { buttonValue, inputText, status, deriveState } = c;
+  const fruit = inputText || buttonValue;
+  console.log(result, "result");
 
-  qna[1].answer = buttonValue || "";
-
+  console.log(c, "c2");
   return c.res({
     action: "/q2",
     image: (
@@ -140,33 +196,91 @@ app.frame("/q1", (c) => {
             lineHeight: 1.4,
             marginTop: 30,
             display: "flex",
+            flexDirection: "row",
             padding: "0 120px",
             whiteSpace: "pre-wrap",
           }}
         >
-          Quiz #1
+          {questions[0].question}
+        </div>
+        <div
+          style={{
+            color: "white",
+            fontSize: 30,
+            fontStyle: "normal",
+            letterSpacing: "-0.025em",
+            lineHeight: 1.4,
+            marginTop: 30,
+            display: "flex",
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          A. {questions[0].options[0]}
+        </div>
+        <div
+          style={{
+            color: "white",
+            fontSize: 30,
+            fontStyle: "normal",
+            letterSpacing: "-0.025em",
+            lineHeight: 1.4,
+            marginTop: 30,
+            display: "flex",
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          B. {questions[0].options[1]}
+        </div>
+        <div
+          style={{
+            color: "white",
+            fontSize: 30,
+            fontStyle: "normal",
+            letterSpacing: "-0.025em",
+            lineHeight: 1.4,
+            marginTop: 30,
+            display: "flex",
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          C. {questions[0].options[2]}
+        </div>
+        <div
+          style={{
+            color: "white",
+            fontSize: 30,
+            fontStyle: "normal",
+            letterSpacing: "-0.025em",
+            lineHeight: 1.4,
+            marginTop: 30,
+            display: "flex",
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          D. {questions[0].options[3]}
         </div>
       </div>
     ),
     intents: [
-      <Button value="oranges">O1</Button>,
-      <Button value="pear" >O2</Button>,
-      <Button value="bananas">O3</Button>,
-      <Button value="bananas">O4</Button>,
+      <Button value={questions[0]?.options[0]}>A</Button>,
+      <Button value={questions[0]?.options[1]}>B</Button>,
+      <Button value={questions[0]?.options[2]}>C</Button>,
+      <Button value={questions[0]?.options[3]}>D</Button>,
+      // status === "response" && <Button.Reset>Reset</Button.Reset>,
     ],
   });
 });
 
 app.frame("/q2", (c) => {
-  const { buttonValue, status } = c;
-  const answer = buttonValue;
-
-  qna[1].answer = answer || "";
-  
-  // console.log("context ->", c);
-
-  console.log("qna ->", qna);
-
+  const { buttonValue, inputText, status, deriveState } = c;
+  const fruit = inputText || buttonValue;
+  console.log(result, "result");
+  result.qn_one = buttonValue;
+  console.log(c, "c2");
   return c.res({
     action: "/q3",
     image: (
@@ -196,21 +310,344 @@ app.frame("/q2", (c) => {
             lineHeight: 1.4,
             marginTop: 30,
             display: "flex",
+            flexDirection: "row",
             padding: "0 120px",
             whiteSpace: "pre-wrap",
           }}
         >
-          hello {answer || ""}
-          {buttonValue === "page2" && "this is page 2"}
-          {buttonValue === "page3" && "this is page 3"}
-          {buttonValue === "page4" && "this is page 4"}
+          {questions[1].question}
+        </div>
+        <div
+          style={{
+            color: "white",
+            fontSize: 30,
+            fontStyle: "normal",
+            letterSpacing: "-0.025em",
+            lineHeight: 1.4,
+            marginTop: 30,
+            display: "flex",
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          A. {questions[1].options[0]}
+        </div>
+        <div
+          style={{
+            color: "white",
+            fontSize: 30,
+            fontStyle: "normal",
+            letterSpacing: "-0.025em",
+            lineHeight: 1.4,
+            marginTop: 30,
+            display: "flex",
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          B. {questions[1].options[1]}
+        </div>
+        <div
+          style={{
+            color: "white",
+            fontSize: 30,
+            fontStyle: "normal",
+            letterSpacing: "-0.025em",
+            lineHeight: 1.4,
+            marginTop: 30,
+            display: "flex",
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          C. {questions[1].options[2]}
+        </div>
+        <div
+          style={{
+            color: "white",
+            fontSize: 30,
+            fontStyle: "normal",
+            letterSpacing: "-0.025em",
+            lineHeight: 1.4,
+            marginTop: 30,
+            display: "flex",
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          D. {questions[1].options[3]}
         </div>
       </div>
     ),
     intents: [
-      <Button value="oranges">Oranges HELLO</Button>,
-      <Button value="bananas">Bananas</Button>,
-      status === "response" && <Button.Reset>Reset</Button.Reset>,
+      <Button value={questions[1]?.options[0]}>A</Button>,
+      <Button value={questions[1]?.options[1]}>B</Button>,
+      <Button value={questions[1]?.options[2]}>C</Button>,
+      <Button value={questions[1]?.options[3]}>D</Button>,
+      // status === "response" && <Button.Reset>Reset</Button.Reset>,
+    ],
+  });
+});
+
+app.frame("/q3", (c) => {
+  const { buttonValue, inputText, status, deriveState } = c;
+  const fruit = inputText || buttonValue;
+  console.log(result, "result");
+  result.qn_two = buttonValue;
+  console.log(c, "c2");
+  return c.res({
+    action: "/q4",
+    image: (
+      <div
+        style={{
+          alignItems: "center",
+          background:
+            status === "response"
+              ? "linear-gradient(to right, #432889, #17101F)"
+              : "black",
+          backgroundSize: "100% 100%",
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "nowrap",
+          height: "100%",
+          justifyContent: "center",
+          textAlign: "center",
+          width: "100%",
+        }}
+      >
+        <div
+          style={{
+            color: "white",
+            fontSize: 60,
+            fontStyle: "normal",
+            letterSpacing: "-0.025em",
+            lineHeight: 1.4,
+            marginTop: 30,
+            display: "flex",
+            flexDirection: "row",
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          {questions[2].question}
+        </div>
+        <div
+          style={{
+            color: "white",
+            fontSize: 30,
+            fontStyle: "normal",
+            letterSpacing: "-0.025em",
+            lineHeight: 1.4,
+            marginTop: 30,
+            display: "flex",
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          A. {questions[2].options[0]}
+        </div>
+        <div
+          style={{
+            color: "white",
+            fontSize: 30,
+            fontStyle: "normal",
+            letterSpacing: "-0.025em",
+            lineHeight: 1.4,
+            marginTop: 30,
+            display: "flex",
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          B. {questions[2].options[1]}
+        </div>
+        <div
+          style={{
+            color: "white",
+            fontSize: 30,
+            fontStyle: "normal",
+            letterSpacing: "-0.025em",
+            lineHeight: 1.4,
+            marginTop: 30,
+            display: "flex",
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          C. {questions[2].options[2]}
+        </div>
+        <div
+          style={{
+            color: "white",
+            fontSize: 30,
+            fontStyle: "normal",
+            letterSpacing: "-0.025em",
+            lineHeight: 1.4,
+            marginTop: 30,
+            display: "flex",
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          D. {questions[2].options[3]}
+        </div>
+      </div>
+    ),
+    intents: [
+      <Button value={questions[2]?.options[0]}>A</Button>,
+      <Button value={questions[2]?.options[1]}>B</Button>,
+      <Button value={questions[2]?.options[2]}>C</Button>,
+      <Button value={questions[2]?.options[3]}>D</Button>,
+      // status === "response" && <Button.Reset>Reset</Button.Reset>,
+    ],
+  });
+});
+
+app.frame("/q4", (c) => {
+  const { buttonValue, inputText, status, deriveState } = c;
+  const fruit = inputText || buttonValue;
+  console.log(result, "result");
+  result.qn_four = buttonValue;
+  console.log(c, "c2");
+  return c.res({
+    action: "/result",
+    image: (
+      <div
+        style={{
+          alignItems: "center",
+          background:
+            status === "response"
+              ? "linear-gradient(to right, #432889, #17101F)"
+              : "black",
+          backgroundSize: "100% 100%",
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "nowrap",
+          height: "100%",
+          justifyContent: "center",
+          textAlign: "center",
+          width: "100%",
+        }}
+      >
+        <div
+          style={{
+            color: "white",
+            fontSize: 60,
+            fontStyle: "normal",
+            letterSpacing: "-0.025em",
+            lineHeight: 1.4,
+            marginTop: 30,
+            display: "flex",
+            flexDirection: "row",
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          {questions[3].question}
+        </div>
+        <div
+          style={{
+            color: "white",
+            fontSize: 30,
+            fontStyle: "normal",
+            letterSpacing: "-0.025em",
+            lineHeight: 1.4,
+            marginTop: 30,
+            display: "flex",
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          A. {questions[3].options[0]}
+        </div>
+        <div
+          style={{
+            color: "white",
+            fontSize: 30,
+            fontStyle: "normal",
+            letterSpacing: "-0.025em",
+            lineHeight: 1.4,
+            marginTop: 30,
+            display: "flex",
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          B. {questions[3].options[1]}
+        </div>
+        <div
+          style={{
+            color: "white",
+            fontSize: 30,
+            fontStyle: "normal",
+            letterSpacing: "-0.025em",
+            lineHeight: 1.4,
+            marginTop: 30,
+            display: "flex",
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          C. {questions[3].options[2]}
+        </div>
+        <div
+          style={{
+            color: "white",
+            fontSize: 30,
+            fontStyle: "normal",
+            letterSpacing: "-0.025em",
+            lineHeight: 1.4,
+            marginTop: 30,
+            display: "flex",
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          D. {questions[3].options[3]}
+        </div>
+      </div>
+    ),
+    intents: [
+      <Button value={questions[3]?.options[0]}>A</Button>,
+      <Button value={questions[3]?.options[1]}>B</Button>,
+      <Button value={questions[3]?.options[2]}>C</Button>,
+      <Button value={questions[3]?.options[3]}>D</Button>,
+      // status === "response" && <Button.Reset>Reset</Button.Reset>,
+    ],
+  });
+});
+
+app.frame("/q4", (c) => {
+  const { buttonValue, inputText, status, deriveState } = c;
+  const fruit = inputText || buttonValue;
+  console.log(result, "result");
+  result.qn_four = buttonValue;
+  console.log(c, "c2");
+  return c.res({
+    action: "/result",
+    image: (
+      <div
+        style={{
+          alignItems: "center",
+          background:
+            status === "response"
+              ? "linear-gradient(to right, #432889, #17101F)"
+              : "black",
+          backgroundSize: "100% 100%",
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "nowrap",
+          height: "100%",
+          justifyContent: "center",
+          textAlign: "center",
+          width: "100%",
+        }}
+      ></div>
+    ),
+    intents: [
+      <Button value={questions[3]?.options[0]}>Like</Button>,
+      <Button value={questions[3]?.options[0]}>Share</Button>,
+      // status === "response" && <Button.Reset>Reset</Button.Reset>,
     ],
   });
 });
--- app/data.ts
+++ app/data.ts
@@ -1,38 +1,38 @@
 export const questions = [
   {
     question: "How do you use Warpcast?",
-    options: {
-      "I share memes and jokes": "shitposter",
-      "I update others with news": "information guru",
-      "I show off my achievements": "flex monster",
-      "I greet everyone daily": "gm officer"
-    }
+    options: [
+      "I share memes and jokes",
+      "I update others with news",
+      "I show off my achievements",
+      "I greet everyone daily",
+    ],
   },
   {
     question: "What's your posting style?",
-    options: {
-      "Sarcastic and humorous": "shitposter",
-      "Informative threads": "information guru",
-      "Flex em wif photos": "flex monster",
-      "Friendly and engaging": "gm officer"
-    }
+    options: [
+      "Sarcastic and humorous",
+      "Informative threads",
+      "Flex em wif photos",
+      "Friendly and engaging",
+    ],
   },
   {
     question: "What's your main content?",
-    options: {
-      "Memes and trolls": "shitposter",
-      "News and updates": "information guru",
-      "Choose rich": "flex monster",
-      "Positive daily interactions": "gm officer"
-    }
+    options: [
+      "Memes and trolls",
+      "News and updates",
+      "Choose rich",
+      "Positive daily interactions",
+    ],
   },
   {
     question: "How do others describe you?",
-    options: {
-      "The class clown": "shitposter",
-      "The news anchor": "information guru",
-      "The show-off": "flex monster",
-      "The morning person": "gm officer"
-    }
-  }
+    options: [
+      "The class clown",
+      "The news anchor",
+      "The show-off",
+      "The morning person",
+    ],
+  },
 ];
